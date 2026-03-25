d6a567b46ce620eebcb917f97c943f3bfd5a976b
MakoVoltron <30395776+MakoVoltron@users.noreply.github.com>
2026-03-25 11:07:15 +0100

    [ADD] You might also like templates

--- app/templates/[slug]/page.tsx
+++ app/templates/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { prisma } from "@/lib/db/client";
 import { cache } from "react";
 import { redirect } from "next/navigation";
 import StripeCheckoutForm from "@/components/StripeCheckoutForm";
+import Link from "next/link";
 
 const getTemplateBySlug = cache((slug: string) =>
   prisma.templatePreview.findUnique({ where: { slug } }),
@@ -35,26 +36,47 @@ export default async function TemplatePage({
     return redirect("/");
   }
 
-  console.log("template");
-  console.log(template);
+  const similarTemplates = await prisma.templatePreview.findMany({
+    where: { category: template.category, id: { not: template.id } },
+    take: 3,
+  });
 
   return (
-    <div className="border max-w-5xl rounded-md bg-gray-900/50">
-      <div className="grid grid-cols-12 gap-2 p-6">
-        <div className="col-span-8 space-y-2">
-          <div className="rounded overflow-hidden">
-            <Video
-              posterUrl={template.posterUrl}
-              videoUrl={template.videoUrl}
-              title={template.title}
-            />
-          </div>
-          <h1 className="text-xl font-bold">{template?.title}</h1>
+    <div className="max-w-5xl space-y-3">
+      <div className="border rounded-md bg-gray-900/50 p-6">
+        <div className="grid grid-cols-12 gap-2">
+          <div className="col-span-8 space-y-2">
+            <div className="rounded overflow-hidden">
+              <Video
+                posterUrl={template.posterUrl}
+                videoUrl={template.videoUrl}
+                title={template.title}
+              />
+            </div>
+            <h1 className="text-2xl font-bold">{template?.title}</h1>
 
-          <p>{template?.description}</p>
+            <p>{template?.description}</p>
+          </div>
+          <div className="col-span-4">
+            <StripeCheckoutForm template={template} />
+          </div>
         </div>
-        <div className="col-span-4">
-          <StripeCheckoutForm template={template} />
+      </div>
+
+      <div className="space-y-1">
+        <h3 className="text-md font-bold">You might also like</h3>
+        <div className="grid grid-cols-12 gap-2">
+          {similarTemplates.map((template) => (
+            <div key={template.id} className="col-span-4">
+              <Link href={`/templates/${template.slug}`}>
+                <Video
+                  posterUrl={template.posterUrl}
+                  videoUrl={template.videoUrl}
+                  title={template.title}
+                />
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </div>
